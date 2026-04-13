03d0f7c40f9c8853c1fa5566d9225dc490a3408e
22sahib22 <ssing791@mtroyal.ca>
2026-04-13 13:07:20 -0600

    enemies drop xp levels still dont do anything

--- characters/Enemy/EnemyType.gd
+++ characters/Enemy/EnemyType.gd
@@ -6,3 +6,4 @@ class_name Enemy
 @export var health : float
 @export var damage : float
 @export var scale : Vector2 = Vector2(1.0, 1.0)
+@export var drops : Array[Pickups]
---
+++ option_slot.gd
@@ -0,0 +1,15 @@
+extends TextureButton
+
+
+@export var weapon : Weapon:
+	set(value):
+		weapon = value
+		texture_normal = value.texture
+		$Label.text = "LVL " + str(weapon.level + 1)
+		
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("click") and weapon:
+		print(weapon.title)
+		get_parent().close_option()
